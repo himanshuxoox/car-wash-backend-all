1429c0a4434a9e02918fde9eb6afffd871774a10
himanshuxoox <87160131+himanshuxoox@users.noreply.github.com>
2026-01-08 19:29:41 +0530

    user service

---
+++ user-service/src/main/java/com/carwash/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carwash.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
